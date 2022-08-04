368f880f9530c4ee6b627cfcbe0712c67bcf5e60
Nicneo95 <neo.hongsheng@trentglobal.edu.sg>
2022-08-04 15:55:47 +0000

    base hbs layout done

--- views/layouts/base.js
+++ views/layouts/base.js
@@ -1,9 +1,6 @@
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-  }
+const menuIconButton = document.querySelector("[data-menu-icon-btn]")
+const sidebar = document.querySelector("[data-sidebar]")
+
+menuIconButton.addEventListener("click", () => {
+  sidebar.classList.toggle("open")
+})
