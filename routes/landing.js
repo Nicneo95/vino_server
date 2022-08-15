c6dbb4466b4d2f143b27169edc0102737ae78484
Nicneo95 <neo.hongsheng@trentglobal.edu.sg>
2022-08-15 12:07:00 +0000

    login landing page done

--- routes/landing.js
+++ routes/landing.js
@@ -2,17 +2,12 @@ const express = require('express');
 const router = express.Router(); // create a new express router
 
 // add a new route to the express router
-router.get('/', (req,res)=>{
-    res.render('landing/index')
-})
-router.get('/order', (req,res)=>{
-    res.render('landing/order')
-})
-router.get('/product', (req,res)=>{
-    res.render('landing/product')
-})
-router.get('/shipment', (req,res)=>{
-    res.render('landing/shipment')
+router.get('/', async (req, res) => {
+    if (req.session.user) {
+        res.redirect('/product-information/product')
+    } else {
+        res.redirect('/user/login')
+    }
 })
 
 module.exports = router; // export out the router
